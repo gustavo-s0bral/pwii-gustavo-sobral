3294175829de0805157a4455dfcf8d0158c4c46e
Gustavo Sobral <gustavosobral1711@gmail.com>
2025-04-09 10:35:55 -0300

    Aula funçoes

---
+++ aula05/funcao.php
@@ -0,0 +1,15 @@
+<?php 
+    function myMensagem(){
+        echo "Hello bebezinho <br>";
+    }
+
+    myMensagem();
+    myMensagem();
+
+    function Soma($n1, $n2){
+        echo $n1 + $n2 , "<br>";
+    }
+
+    Soma(13 + 4);
+    Soma(10 + 4);
+?>
